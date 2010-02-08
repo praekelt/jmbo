ff86710494a6d658e39ff0cbb458f69d48c63b3b
Shaun Sephton <shaun@praekelt.com>
2010-02-08 20:04:07 +0200

    added admin models

---
+++ content/admin.py
@@ -0,0 +1,32 @@
+from copy import deepcopy
+
+from django.contrib import admin
+
+class ModelBaseAdmin(admin.ModelAdmin):
+    list_display = ('is_public',)
+    list_filter = ('is_public',)
+    fieldsets = (
+        (None, {'fields': ('is_public',)}),
+    )
+
+class ContentBaseAdmin(ModelBaseAdmin):
+    list_display = ('title', 'owner', 'created', 'modified', 'admin_thumbnail') + ModelBaseAdmin.list_display
+    list_filter = ('created', 'modified',) + ModelBaseAdmin.list_filter
+    search_fields = ('title', 'description')
+   
+    fieldsets = deepcopy(ModelBaseAdmin.fieldsets)
+    for fieldset in fieldsets:
+        if fieldset[0] == None:
+            fieldset[1]['fields'] += ('title', 'description',)
+
+    fieldsets += (
+        ('Meta', {'fields': ('image', 'created', 'owner', 'rating',),
+                  'classes': ('collapse',),
+        }),
+    )
+   
+    def save_model(self, request, obj, form, change):
+        if not obj.owner:
+            obj.owner = request.user
+            obj.save()
+        return super(ContentBaseAdmin, self).save_model(request, obj, form, change)
