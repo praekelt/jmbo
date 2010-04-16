aa62e1ac5357e21bad767fc7fc55c8b881ca7bc5
Shaun Sephton <shaun@praekelt.com>
2010-04-16 13:07:17 +0200

    nice fieldsets, listing and filters

--- content/admin.py
+++ content/admin.py
@@ -3,16 +3,22 @@ from django.contrib.auth.models import User
 
 from content.models import ModelBase
 
-
 class ModelBaseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'state', 'admin_thumbnail', 'owner', 'created')
+    list_filter = ('state', 'created')
+    search_fields = ('title', 'description', 'state', 'created')
+    fieldsets = (
+        (None, {'fields': ('state', 'title', 'description', )}),
+        ('Meta', {'fields': ('categories', 'tags', 'created', 'owner'),
+                    'classes': ('collapse',),
+        }),
+        ('Image', {'fields': ('image', 'crop_from', 'effect'),
+                    'classes': ('collapse',),
+        }),
+    )
+    
     def save_model(self, request, obj, form, change):
-        try:
-            obj.owner
-        except User.DoesNotExist:
+        if not obj.owner:
             obj.owner = request.user
       
-        obj.save()
-            
         return super(ModelBaseAdmin, self).save_model(request, obj, form, change)
-
-admin.site.register(ModelBase, ModelBaseAdmin)
