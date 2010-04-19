6148f2ee2a827eb8eb2ae3530fa59d393ca87a4a
Shaun Sephton <shaun@praekelt.com>
2010-04-19 13:42:38 +0200

    filter on site

--- content/managers.py
+++ content/managers.py
@@ -3,7 +3,13 @@ from django.db import models
 
 class PermittedManager(models.Manager):
     def get_query_set(self):
+        # get base queryset and exclude based on state
         queryset = super(PermittedManager, self).get_query_set().exclude(state='unpublished')
+
+        # exclude objects in staging state if not in staging mode (settings.STAGING = False)
         if getattr(settings, 'STAGING', False):
             queryset = queryset.exclude(state='staging')
+
+        # filter objects for current site 
+        queryset = queryset.filter(sites__id__exact=settings.SITE_ID)
         return queryset
