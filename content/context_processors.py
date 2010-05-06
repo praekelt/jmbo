3eaf3896465015fe90904a4d4c4543e9bd33986d
Shaun Sephton <shaun@praekelt.com>
2010-05-06 14:22:25 +0200

    added site cp

---
+++ content/context_processors.py
@@ -0,0 +1,8 @@
+from django.contrib.sites.models import Site
+
+def site(request):
+    try:
+        site = Site.objects.get_current()
+    except Site.DoesNotExist:
+        site = None
+    return {'site': site} 
