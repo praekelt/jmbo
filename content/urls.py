4da801dd1bb9b975c109610bc7a71f4e65e3eefc
Shaun Sephton <shaun@praekelt.com>
2010-05-06 09:41:16 +0200

    initial content url work

---
+++ content/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls.defaults import patterns, url
+
+urlpatterns = patterns(
+    'content.views',
+    url(r'^(?P<category_slug>[\w-]+)/list/$', 'category_object_list', name='content_category_object_list'),
+    url(r'^(?P<category_slug>[\w-]+)/(?P<slug>[\w-]+)/$', 'category_object_detail', name='content_category_object_detail'),
+)
