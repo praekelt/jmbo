d057dd79158851e97c100344ac6738a3ba2c0cc2
shaunsephton <shaunsephton@gmail.com>
2010-05-12 15:08:04 +0200

    filter for self

--- content/utils/__init__.py
+++ content/utils/__init__.py
@@ -1,18 +1,17 @@
 from django.db.utils import IntegrityError
 from django.template.defaultfilters import slugify
 
-
 def generate_slug(obj, text, tail_number=0):
     from content.models import ModelBase
     """
-    Sets a slug on object. Object must provide a SlugField called slug.
+    Returns a new unique slug. Object must provide a SlugField called slug.
     URL friendly slugs are generated using django.template.defaultfilters' slugify.
     Numbers are added to the end of slugs for uniqueness.
     """
     # use django slugify filter to slugify
     slug = slugify(text)
 
-    existing_slugs = [item.slug for item in ModelBase.objects.all()]
+    existing_slugs = [item.slug for item in ModelBase.objects.exclude(id=obj.id)]
 
     tail_number = 0
     new_slug = slug
