274385ee1d625c5a5c2a1e56e60a15003af80eae
shaunsephton <shaunsephton@gmail.com>
2010-06-24 08:40:21 +0200

    switched to updated views, use view modifiers

--- panya/views.py
+++ panya/views.py
@@ -4,40 +4,34 @@ from django.shortcuts import get_object_or_404
 from category.models import Category
 from panya.generic.views import GenericObjectDetail, GenericObjectList
 from panya.models import ModelBase
-from panya.pagemenus import ContentPageMenu
+from panya.view_modifiers import DefaultViewModifier
 
 class CategoryURL(object):
     def __init__(self, category):
         self.category = category
 
-    def __call__(self, obj):
-        return reverse('content_category_object_detail', kwargs={'category_slug': self.category.slug, 'slug': obj.slug})
+    def __call__(self, obj=None):
+        if obj:
+            return reverse('content_category_object_detail', kwargs={'category_slug': self.category.slug, 'slug': obj.slug})
+        else:
+            return self
 
 class CategoryObjectList(GenericObjectList):
-    def get_queryset(self):
+    def get_queryset(self, *args, **kwargs):
         return ModelBase.permitted.filter(categories=self.category)
    
-    def get_pagemenu(self, request, queryset, *args, **kwargs):
-        return ContentPageMenu(queryset, request)
+    def get_view_modifier(self, request, *args, **kwargs):
+        return DefaultViewModifier(request)
 
-    def get_paginate_by(self):
+    def get_paginate_by(self, *args, **kwargs):
         return 7
 
-    def get_url_callable(self):
+    def get_url_callable(self, *args, **kwargs):
         return CategoryURL(category=self.category)
    
     def get_extra_context(self, *args, **kwargs):
-        extra_context = super(CategoryObjectList, self).get_extra_context(*args, **kwargs)
-        added_context = {'title': self.category.title }
-        if extra_context:
-            extra_context.update(
-                added_context,
-            )
-        else:
-            extra_context = added_context
-
-        return extra_context
-    
+        return {'title': self.category.title }
+   
     def __call__(self, request, category_slug, *args, **kwargs):
         self.category = get_object_or_404(Category, slug__iexact=category_slug)
         return super(CategoryObjectList, self).__call__(request, *args, **kwargs)
@@ -48,19 +42,11 @@ class CategoryObjectDetail(GenericObjectDetail):
     def get_queryset(self, *args, **kwargs):
         return ModelBase.permitted.filter(categories=self.category)
     
-    def get_pagemenu(self, request, queryset, *args, **kwargs):
-        return ContentPageMenu(queryset, request)
+    def get_view_modifier(self, request, *args, **kwargs):
+        return DefaultViewModifier(request, base_url=reverse('content_category_object_list', kwargs={'category_slug': 'news-updates'}), ignore_defaults=True)
     
     def get_extra_context(self, *args, **kwargs):
-        extra_context = super(CategoryObjectDetail, self).get_extra_context(*args, **kwargs)
-        added_context = {'title': self.category.title }
-        if extra_context:
-            extra_context.update(
-                added_context,
-            )
-        else:
-            extra_context = added_context
-        return extra_context
+        return {'title': self.category.title }
     
     def __call__(self, request, category_slug, *args, **kwargs):
         self.category = get_object_or_404(Category, slug__iexact=category_slug)
