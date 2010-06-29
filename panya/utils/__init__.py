b219f28dd5651c1d9ce210227b2597c6ffc1971f
Euan Jonker <euan@praekelt.com>
2010-06-29 17:29:52 +0200

    Added mini searching ability.

--- panya/utils/__init__.py
+++ panya/utils/__init__.py
@@ -1,3 +1,6 @@
+import re
+
+from django.db.models import Q
 from django.db.utils import IntegrityError
 from django.template.defaultfilters import slugify
 
@@ -20,3 +23,41 @@ def generate_slug(obj, text, tail_number=0):
         tail_number += 1
 
     return new_slug
+
+#------------------------------------------------------------------------------
+def normalize_query(query_string,
+                    findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
+                    normspace=re.compile(r'\s{2,}').sub):
+    """
+    Splits the query string in invidual keywords, getting rid of unecessary spaces
+    and grouping quoted words together.
+    Example:
+      
+    >>> normalize_query('  some random  words "with   quotes  " and   spaces')
+    ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
+    """
+    
+    return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)] 
+
+#------------------------------------------------------------------------------
+def get_query(query_string, search_fields):
+    """
+    Returns a query, that is a combination of Q objects. That combination
+    aims to search keywords within a model by testing the given search fields.
+    search_fields should be something like: [('title', 'iexact'), ('content', 'icontains'), ]
+    """
+    query = None # Query to search for every search term        
+    terms = normalize_query(query_string)
+    for term in terms:
+        or_query = None # Query to search for a given term in each field
+        for field in search_fields:
+            q = Q(**{"%s__%s" % (field[0], field[1]): term})
+            if or_query is None:
+                or_query = q
+            else:
+                or_query = or_query | q
+        if query is None:
+            query = or_query
+        else:
+            query = query & or_query
+    return query
