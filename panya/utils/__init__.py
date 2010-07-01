df486c01af33718c50fa2117d80f2edf84678bc8
shaunsephton <shaunsephton@gmail.com>
2010-07-01 15:04:40 +0200

    added modify class util

--- panya/utils/__init__.py
+++ panya/utils/__init__.py
@@ -1,3 +1,6 @@
+import inspect
+import types
+
 from django.db.utils import IntegrityError
 from django.template.defaultfilters import slugify
 
@@ -20,3 +23,21 @@ def generate_slug(obj, text, tail_number=0):
         tail_number += 1
 
     return new_slug
+
+def modify_class(original_class, modifier_class, override=True):
+    """
+    Adds class methods from modifier_class to original_class.
+    If override is True existing methods in original_class are overriden by those provided by modifier_class.
+    """
+    # get members to add
+    modifier_methods = inspect.getmembers(modifier_class, inspect.ismethod)
+    
+    # set methods
+    for method_tuple in modifier_methods:
+        name = method_tuple[0]
+        method = method_tuple[1]
+        if isinstance(method, types.UnboundMethodType):
+            if hasattr(original_class, name) and not override:
+                return None
+            else:
+                setattr(original_class, name, method.im_func)
