c336df761f8a2075048243cf9d0a682c93c29850
shaunsephton <shaunsephton@gmail.com>
2010-08-05 12:38:32 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='panya',
-    version='0.0.9',
+    version='0.0.10',
     description='Panya base app.',
     long_description = open('README.rst', 'r').read(),
     author='Praekelt Foundation',
