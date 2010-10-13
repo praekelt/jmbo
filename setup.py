329c303e27941dcf109d93a91e4accc3b1a88f7c
shaunsephton <shaun@praekeltconsulting.com>
2010-10-13 10:18:28 +0200

    use photologue 2.5.praekelt. version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='panya',
-    version='0.1.0',
+    version='0.1.1',
     description='Panya base app.',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Foundation',
@@ -12,12 +12,12 @@ setup(
     packages = find_packages(),
     dependency_links = [
         'http://dist.plone.org/thirdparty/',
-        'http://github.com/praekelt/django-photologue/tarball/2.4.praekelt#egg=django-photologue-2.4.praekelt',
+        'http://github.com/praekelt/django-photologue/tarball/2.5.praekelt#egg=django-photologue-2.5.praekelt',
     ],
     install_requires = [
         'PIL',
         'django-category',
-        'django-photologue==2.4.praekelt',
+        'django-photologue==2.5.praekelt',
         'django-publisher',
         'django-secretballot',
     ],
