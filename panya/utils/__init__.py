215013a61f7d9673b0e6e2980a175ea31fcc0c21
Simon de Haan <simon@praekeltfoundation.org>
2011-01-14 16:50:43 +0200

    if no longer needed

--- panya/utils/__init__.py
+++ panya/utils/__init__.py
@@ -16,10 +16,7 @@ def generate_slug(obj, text, tail_number=0):
     # use django slugify filter to slugify
     slug = slugify(text)
     
-    if slug:
-        existing_slugs = [item.slug for item in ModelBase.objects.filter(slug__regex=r'^%s' % slug).exclude(id=obj.id)]
-    else:
-        existing_slugs = [item.slug for item in ModelBase.objects.filter(slug__regex=r'^(-\d+)?').exclude(id=obj.id)]
+    existing_slugs = [item.slug for item in ModelBase.objects.filter(slug__regex=r'^(-\d+)?').exclude(id=obj.id)]
     
     tail_number = 0
     new_slug = slug
