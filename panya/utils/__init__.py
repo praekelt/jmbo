a289eb71e5efc5cc76cc16c48bbbc858a67f6d30
Simon de Haan <simon@praekeltfoundation.org>
2011-01-14 15:36:14 +0200

    reduces postgresql query response time from ~50ms to anywhere between 0.1 and 1.5ms

--- panya/utils/__init__.py
+++ panya/utils/__init__.py
@@ -15,9 +15,12 @@ def generate_slug(obj, text, tail_number=0):
     """
     # use django slugify filter to slugify
     slug = slugify(text)
-
-    existing_slugs = [item.slug for item in ModelBase.objects.filter(slug__startswith=slug).exclude(id=obj.id)]
-
+    
+    if slug:
+        existing_slugs = [item.slug for item in ModelBase.objects.filter(slug__regex=r'^%s' % slug).exclude(id=obj.id)]
+    else:
+        existing_slugs = [item.slug for item in ModelBase.objects.filter(slug='').exclude(id=obj.id)]
+    
     tail_number = 0
     new_slug = slug
     while new_slug in existing_slugs:
