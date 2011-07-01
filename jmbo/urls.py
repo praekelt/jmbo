c8160c6876121aa6411c5244651e7acc0f601dd9
Shaun Sephton <shaunsephton@gmail.com>
2011-07-01 15:07:50 +0200

    added index. more easily include

--- jmbo/urls.py
+++ jmbo/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import patterns, url
+from django.views.generic import TemplateView
 
 urlpatterns = patterns(
     'jmbo.views',
-    url(r'^peek/(?P<slug>[\w-]+)/$', 'object_peek', name='object_peek'),
-    url(r'^(?P<category_slug>[\w-]+)/list/$', 'category_object_list', name='content_category_object_list'),
-    url(r'^(?P<category_slug>[\w-]+)/(?P<slug>[\w-]+)/$', 'category_object_detail', name='content_category_object_detail'),
+    url(r'^$', TemplateView.as_view(template_name="jmbo/index.html"), name='index'),
+    url(r'^content/peek/(?P<slug>[\w-]+)/$', 'object_peek', name='object_peek'),
+    url(r'^content/(?P<category_slug>[\w-]+)/list/$', 'category_object_list', name='content_category_object_list'),
+    url(r'^content/(?P<category_slug>[\w-]+)/(?P<slug>[\w-]+)/$', 'category_object_detail', name='content_category_object_detail'),
 )
