e67eeeed376bfd417f750e129853e73859c94032
hedleyroos <hedleyroos@gmail.com>
2011-09-03 10:44:39 +0200

    Likes enabled test moved to a signal handler

--- jmbo/signals.py
+++ jmbo/signals.py
@@ -1,11 +1,17 @@
 from django.dispatch import receiver
 
-from likes.signals import can_vote_test
-from likes.exceptions import CannotVoteException
+from likes.signals import likes_enabled_test, can_vote_test
+from likes.exceptions import CannotVoteException, LikesNotEnabledException
+
+@receiver(likes_enabled_test)
+def on_likes_enabled_test(sender, request, **kwargs):
+    if not sender.likes_enabled:
+        raise LikesNotEnabledException()
+    return True
 
 @receiver(can_vote_test)
 def on_can_vote_test(sender, user, request, **kwargs):
     result, extra = sender.can_vote(request)
-    if result:
+    if not result:
         raise CannotVoteException(extra)
     return result, extra
