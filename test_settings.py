fa05047a6aa9d7158763f7070e137893f1989629
Shaun Sephton <shaunsephton@gmail.com>
2011-09-05 09:33:49 +0200

    testrunner

---
+++ test_settings.py
@@ -0,0 +1,14 @@
+DATABASE_ENGINE = 'sqlite3'
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.comments',
+    'django.contrib.contenttypes',
+    'django.contrib.sites',
+    'category',
+    'jmbo',
+    'photologue',
+    'secretballot',
+]
+
+SITE_ID = 0
