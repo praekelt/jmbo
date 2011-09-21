8ebf1f1a5c5a65904f34afe5ed7261e77c22e9b4
Shaun Sephton <shaunsephton@gmail.com>
2011-09-21 11:43:31 +0200

    remed generic templates, moved to jmbo-generic

--- jmbo/urls.py
+++ jmbo/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls.defaults import patterns, url
-from django.views.generic import TemplateView
 
 urlpatterns = patterns(
     'jmbo.views',
-    url(r'^$', TemplateView.as_view(template_name="jmbo/index.html"), \
-            name='index'),
     url(r'^content/peek/(?P<slug>[\w-]+)/$', 'object_peek', \
             name='object_peek'),
     url(r'^content/(?P<category_slug>[\w-]+)/list/$', 'category_object_list', \
