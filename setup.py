cda55f76135b84107950dcadb7ead08a08e0c2d1
hedleyroos <hedleyroos@gmail.com>
2011-10-19 12:14:34 +0200

    Add version dependency

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires = [
         'PIL',
         'Django>=1.3',
-        'django-category',
+        'django-category>=0.0.5',
         'django-photologue==2.6.praekelt',
         'django-preferences',
         'django-publisher',
