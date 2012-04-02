c186d5c30f5858bd85e034044a08958922f58e4b
hedleyroos <hedleyroos@gmail.com>
2012-04-02 14:22:10 +0200

    Pep8 cleanup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jmbo',
-    version='0.1.20 unreleased',
+    version='0.1.20',
     description='Jmbo base app.',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Foundation',
