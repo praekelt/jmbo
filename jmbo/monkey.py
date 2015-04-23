989e17799f1c6deacc4c5018873acc60866670ed
hedleyroos <hedleyroos@gmail.com>
2015-04-23 16:13:10 +0200

    Patches to help out photologue with null image

--- jmbo/monkey.py
+++ jmbo/monkey.py
@@ -7,3 +7,15 @@ from photologue.models import ImageModel, PhotoSize
 ImageModel._meta.get_field("image").blank = True
 ImageModel._meta.get_field("image").null = True
 PhotoSize._meta.get_field("name").max_length = 64
+
+
+# More patches to handle null image
+def ImageModel_decorator(func):
+    def new(self, *args, **kwargs):
+        if not self.image:
+            return
+        return func(self, *args, **kwargs)
+    return new
+
+ImageModel.create_size = ImageModel_decorator(ImageModel.create_size)
+ImageModel.remove_size = ImageModel_decorator(ImageModel.remove_size)
