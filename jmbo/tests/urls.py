36545cc9fa0488a177ffe999f092e1f0b7cc90bb
hedleyroos <hedleyroos@gmail.com>
2016-06-18 12:35:25 +0200

    Remove redundant imports

--- jmbo/tests/urls.py
+++ jmbo/tests/urls.py
@@ -1,8 +1,5 @@
-from importlib import import_module
-
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.conf import settings
 
 from jmbo.views import ObjectDetail, ObjectList
 
