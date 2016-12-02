a82c686c84dd03e8512b8ce9baefc70ea855ae16
hedleyroos <hedleyroos@gmail.com>
2016-12-02 13:30:56 +0200

    Ensure migration is not applied repeatedly

--- jmbo/migrations/0004_photosize_name_length.py
+++ jmbo/migrations/0004_photosize_name_length.py
@@ -4,6 +4,16 @@ from __future__ import unicode_literals
 
 from django.core.validators import RegexValidator
 from django.db import migrations, models
+from django.utils import timezone
+
+
+# We're modifying a field from another app. This requires trickery.
+
+def fix_app_label(apps, schema_editor):
+    migrations.recorder.MigrationRecorder.Migration.objects.create(
+        app='jmbo', name='0004_photosize_name_length',
+        applied=timezone.now()
+    )
 
 
 class Migration(migrations.Migration):
@@ -17,6 +27,7 @@ class Migration(migrations.Migration):
             name='name',
             field=models.CharField(unique=True, max_length=255, validators=[RegexValidator(regex='^[a-z0-9_]+$', message='Use only plain lowercase letters (ASCII), numbers and underscores.')]),
         ),
+        migrations.RunPython(fix_app_label)
     ]
 
     def __init__(self, *args, **kwargs):
